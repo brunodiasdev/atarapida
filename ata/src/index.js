2a71cb421ee1d9324fc655b869adc68efbe5d501
Bruno Dias <brunodiasdevs@gmail.com>
2021-06-05 14:49:32 -0300

    novo

---
+++ ata/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './styles/index.css'
+import App from './App'
+
+const ata = document.getElementById('app')
+ReactDOM.render(<app/>, ata);
+
+

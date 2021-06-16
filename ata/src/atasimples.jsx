56e6dca0386b8d2972bf4914be133a7a22e143b7
Bruno Dias <brunodiasdevs@gmail.com>
2021-06-15 22:46:39 -0300

    n

--- ata/src/atasimples.jsx
+++ ata/src/atasimples.jsx
@@ -15,10 +15,6 @@ return(
             <label for="ffilial">Filial/Unidade:</label><br/>
             <input class="barratxt" type="text" id="ffilial" name="ffilial"/>
         </div>
-        <div className="col-6">
-            <label for="fpresença">Lista de presença:</label><br/>
-            <input class="barratxt" type="text" id="fpresença" name="fpresença"/>
-        </div>
     </div>
 </div>
 
@@ -47,14 +43,35 @@ return(
         </div>
     </div>
 </div>
+<div className="conteiner">
+    <div className="row superbarratxt">
+        <div className="col-9">
+            <label for="fata">Ata:</label><br/>
+            <textarea class="barratxt" type="text" id="fata" name="fata"></textarea>
+        </div>
+        <div className="col-3">
+            <label for="fpresenca">Lista de presença:</label><br/>
+            <textarea class="barratxt" type="text" id="fpresenca" name="fpresenca"></textarea>
+        </div>
+    </div>
+</div>
+<div className="conteiner">
+    <div className="row superbarratxt">
+        <div className="col-9">
+            <button>O</button>
+            <button>O</button>
+            <button>O</button>
+            <button>O</button>
+            <button>O</button>
+        </div>
+        <div className="col-3">
+            <button className="col-6">Exportar PDF</button>
+            <button className="col-6">Exportar Word</button>
+        </div>
+    </div>
+</div>
     
 
-    
-    
-    
-    <h1>Simples</h1>
-    <h1>Simples</h1>
-    <h1>Simples</h1>
 </>)
 }
 
